193a1c5a9be97e36fc3b173ed33acf2f6a64df1f
Girafepothame <loic.yolo@hotmail.fr>
2024-11-06 11:36:04 +0100

    TP1 - 3.4

--- src/app/etudiant/etudiant.component.ts
+++ src/app/etudiant/etudiant.component.ts
@@ -7,6 +7,16 @@ import { Component } from '@angular/core';
   templateUrl: './etudiant.component.html',
   styleUrl: './etudiant.component.scss'
 })
-export class EtudiantComponent {
 
+export class EtudiantComponent {
+  nomEtu = ""
+  prenomEtu = ""
+
+  retNomEtu(nom: string) {
+    return "Nom: " + nom
+  }
+
+  retPrenomEtu(prenom: string) {
+    return "Prénom: " + prenom
+  }
 }
